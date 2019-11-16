e001d88b0afb0a24c2b5fbcf6409c875f1d0a863
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2019-11-16 13:43:53 +0100

    Draw test scene using SDL2

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,10 +1,63 @@
-fn main() {
-    test_scene();
+use raytracer::renderer::DrawCanvas;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use std::time::Duration;
+
+const WINDOW_WIDTH: u32 = 1024;
+const WINDOW_HEIGHT: u32 = 576;
+const CANVAS_WIDTH: u32 = 640;
+const CANVAS_HEIGHT: u32 = 360;
+
+struct WrapperCanvas<'a>(&'a mut Canvas<Window>);
+
+impl<'a> DrawCanvas for WrapperCanvas<'a> {
+    fn draw(&mut self, x: u32, y: u32, color: &raytracer::primitives::Color) {
+        let draw_color = sdl2::pixels::Color::RGB(
+            (255.0 * color.red) as u8,
+            (255.0 * color.green) as u8,
+            (255.0 * color.blue) as u8,
+        );
+        self.0.set_draw_color(draw_color);
+        self.0
+            .draw_point(sdl2::rect::Point::new(x as i32, y as i32));
+    }
 }
 
-fn test_scene() {
-    use raytracer::primitives::{Camera, Sphere, Vec3, Collision};
-    use raytracer::renderer::{Scene, StdoutCanvas, render, RenderOptions};
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("RayTracer Test", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .resizable()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
+    canvas.set_logical_size(CANVAS_WIDTH, CANVAS_HEIGHT);
+    let mut wrapper_canvas = WrapperCanvas(&mut canvas);
+    draw_test_scene(&mut wrapper_canvas);
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'main_loop: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'main_loop,
+                _ => {}
+            }
+        }
+    }
+}
+
+fn draw_test_scene(canvas: &mut impl DrawCanvas) {
+    use raytracer::primitives::{Camera, Collision, Sphere, Vec3};
+    use raytracer::renderer::{render, RenderOptions, Scene, StdoutCanvas};
 
     let camera: Camera = Default::default();
     let sphere: Sphere = Sphere {
@@ -15,7 +68,9 @@ fn test_scene() {
     objects.push(Box::new(sphere));
 
     let scene: Scene = Scene { camera, objects };
-    let canvas = StdoutCanvas;
-    let render_options = RenderOptions { canvas_width: 16, canvas_height: 9 };
-    render(&scene, &canvas, &render_options);
+    let render_options = RenderOptions {
+        canvas_width: CANVAS_WIDTH,
+        canvas_height: CANVAS_HEIGHT,
+    };
+    render(&scene, canvas, &render_options);
 }
