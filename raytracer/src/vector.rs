6a8b51155ac718df584c49532b075fece7dc8b06
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2019-11-30 18:41:11 +0100

    A bit of TDD to implement matrices

--- raytracer/src/vector.rs
+++ raytracer/src/vector.rs
@@ -76,3 +76,191 @@ impl std::ops::Mul<Vec3> for f64 {
         }
     }
 }
+
+#[derive(Debug, Copy, Clone)]
+pub struct Mat3([[f64; 3]; 3]);
+
+impl Mat3 {
+    fn new() -> Self {
+        Mat3([[0.0; 3]; 3])
+    }
+
+    #[rustfmt::skip]
+    fn id() -> Self {
+        Mat3([
+            [1.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 0.0, 1.0]
+        ])
+    }
+}
+
+impl std::ops::Add for Mat3 {
+    type Output = Mat3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        unimplemented!()
+    }
+}
+
+impl std::ops::Mul<Mat3> for f64 {
+    type Output = Mat3;
+
+    fn mul(self, rhs: Mat3) -> Self::Output {
+        unimplemented!()
+    }
+}
+
+impl std::ops::Mul<Vec3> for Mat3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        unimplemented!()
+    }
+}
+
+impl std::ops::Mul<Mat3> for Mat3 {
+    type Output = Mat3;
+
+    fn mul(self, rhs: Mat3) -> Self::Output {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[rustfmt::skip]
+    mod mat {
+        use super::super::*;
+
+        impl std::cmp::PartialEq for Mat3 {
+            fn eq(&self, other: &Self) -> bool {
+                use std::f64::EPSILON;;
+                let mut zipped_iter = self.0.iter().flatten().zip(other.0.iter().flatten());
+                zipped_iter.all(|(&left, &right)| left <= right + EPSILON && left >= right - EPSILON)
+            }
+        }
+
+        impl std::cmp::PartialEq for Vec3 {
+            fn eq(&self, other: &Self) -> bool {
+                use std::f64::EPSILON;;
+                self.x <= other.x + EPSILON && self.x >= other.x - EPSILON
+                && self.y <= other.y + EPSILON && self.y >= other.y - EPSILON
+                && self.z <= other.z + EPSILON && self.z >= other.z - EPSILON
+            }
+        }
+
+        #[test]
+        fn new_mat3_is_zeroed() {
+            // Given a new matrix
+            let mat = Mat3::new();
+            // Then all elements are 0
+            mat.0
+                .iter()
+                .flatten()
+                .for_each(|&val| assert!(val.abs() < std::f64::EPSILON));
+        }
+
+        #[test]
+        fn addition_example() {
+            let mat_1 = Mat3([
+                [0.0, 1.0, 2.0],
+                [3.0, 4.0, 5.0],
+                [6.0, 7.0, 8.0]
+            ]);
+
+            let mat_2 = Mat3([
+                [0.0, 1.0, -5.0],
+                [3.0, 0.0, 5.0],
+                [30.0, -2.0, 1.0]
+            ]);
+            let target = Mat3([
+                [0.0, 2.0, -3.0],
+                [6.0, 4.0, 10.0],
+                [36.0, 5.0, 9.0]
+            ]);
+            let result = mat_1 + mat_2;
+            assert_eq!(target, result);
+        }
+
+        #[test]
+        fn multiply_with_constant_example() {
+            let mat = Mat3([
+                [0.0, 1.0, 2.0],
+                [3.0, 4.0, 5.0],
+                [6.0, 7.0, 8.0]
+            ]);
+            let target =  Mat3([
+                [0.0, 3.0, 6.0],
+                [9.0, 12.0, 15.0],
+                [18.0, 21.0, 24.0]
+            ]);
+            let result = 3.0 * mat;
+            assert_eq!(target, result);
+        }
+
+        #[test]
+        fn multiply_with_matrix_example() {
+                let mat_1 = Mat3([
+                [0.0, 1.0, 2.0],
+                [-1.0, -2.0, 0.0],
+                [10.0, 20.0, -100.0]
+            ]);
+                let mat_2 = Mat3([
+                [0.0, 1.0, 2.0],
+                [3.0, 4.0, 5.0],
+                [6.0, 7.0, 8.0]
+            ]);
+                let target =  Mat3([
+                [15.0, 18.0, 21.0],
+                [46.0, -9.0, -12.0],
+                [-540.0, -610.0, -680.0]
+            ]);
+            let result = mat_1 * mat_2;
+            assert_eq!(target, result);
+        }
+
+        #[test]
+        fn multiply_with_vec3_example() {
+            let mat = Mat3([
+                    [0.0, 1.0, 2.0],
+                    [3.0, 4.0, 5.0],
+                    [6.0, 7.0, 8.0]
+                ]);
+
+            let vec = Vec3::new(-10.0, 0.0, 22.0);
+            let target = Vec3::new(44.0, 80.0, 116.0);
+            let result = mat * vec;
+            assert_eq!(target, result);
+        }
+
+        #[test]
+        fn left_multiply_with_matrix_id_is_same() {
+            let mat = Mat3([
+                [0.0, 1.0, 2.0],
+                [3.0, 4.0, 5.0],
+                [6.0, 7.0, 8.0]
+            ]);
+            let result = mat * Mat3::id();
+            assert_eq!(mat, result);
+        }
+
+        #[test]
+        fn right_multiply_with_matrix_id_is_same() {
+            let mat = Mat3([
+                [0.0, 1.0, 2.0],
+                [3.0, 4.0, 5.0],
+                [6.0, 7.0, 8.0]
+            ]);
+            let result = Mat3::id() * mat;
+            assert_eq!(mat, result);
+        }
+
+        #[test]
+        fn mat3_id_with_vec3_is_vec3() {
+            let vec = Vec3::new(-10.0, 0.0, 22.0);
+            let result = Mat3::id() * vec;
+            assert_eq!(vec, result);
+        }
+    }
+}
