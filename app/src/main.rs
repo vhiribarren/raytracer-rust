02ef995710f40ce5f522d93e48eda2d5cad43673
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2019-12-01 09:10:04 +0100

    Add license notice everywhere

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2019 Vincent Hiribarren
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 mod utils;
 
 use raytracer::renderer::DrawCanvas;
@@ -97,7 +121,7 @@ fn draw_test_scene(canvas: &mut impl DrawCanvas) -> RaytracingResult {
         Vec3::new(0.0, 0.0, 0.0),
         16.0 * 3.0,
         9.0 * 3.0,
-        (PI / 32.0) as f64,
+        (PI / 4.0) as f64,
     );
     let camera = camera_perspective;
     let light = LightPoint {
